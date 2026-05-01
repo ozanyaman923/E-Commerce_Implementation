2a4a3438f5224d6a9e1319342441ca934fc428fb
Ozan Yaman <Ozan Yaman@OZANYAMAN>
2026-05-01 17:37:47 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework.EfEntities;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IProductService productService = new ProductManager(new EfProductDal());
+            foreach (var product in productService.GetByUnitQuery())
+            {
+                Console.WriteLine(product.ProductName + " ------- " + product.UnitPrice);
+            }
+        }
+    }
+} 
---
+++ DataAccess/Concrete/EntityFramework/EfEntities/EfCategoryDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.EfEntities
+{
+    public class EfCategoryDal : ICategoryDal
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Category> IEntityRepository<Category>.GetAllByCategory(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
