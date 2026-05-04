0f4b96041e48e9d951ce9e159f18e471bb09a46b
Ozan Yaman <Ozan Yaman@OZANYAMAN>
2026-05-04 23:18:05 +0300

    -Core katmanı, DTO,Repolar eklendi

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,12 +1,12 @@
-﻿using Entities.Abstract;
-using Entities.Concrete;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
+    //Herhangi bir entity'nin operasyonları
     public interface IEntityRepository<T> where T : class , IEntity, new()
     {
         List<T> GetAll(Expression<Func<T,bool>>filter = null);
@@ -14,6 +14,7 @@ namespace DataAccess.Abstract
         void Add(T entity);
         void Delete(T entity);
         void Update(T entity);
-        List<T> GetAllByCategory(int entityId); 
+       
     }
 }
+ 
--- DataAccess/Concrete/EntityFramework/EfEntities/EfOrderDal.cs
+++ DataAccess/Concrete/EntityFramework/EfEntities/EfOrderDal.cs
@@ -1,18 +1,14 @@
-﻿using Core.DataAccess;
-using Core.DataAccess.EntityFramework;
+﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework.DataBase;
 using Entities.Concrete;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework.EfEntities
 {
-    public class EfCategoryDal : EfEntityRepositoryBase<Category, NorthwindContext>, ICategoryDal
+    public class EfOrderDal : EfEntityRepositoryBase<Order, NorthwindContext>, IOrderDal
     {
-
     }
 }
